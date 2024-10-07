fb9b7ed7a1e515057da9e97a185f35dd1d7fdf31
EvgeniySuharev <169095875+EvgeniySuharev@users.noreply.github.com>
2024-10-07 18:42:16 +0300

    Update module_16_3.py

--- module_16_3.py
+++ module_16_3.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 
 app = FastAPI()
@@ -12,14 +12,19 @@ async def get_users() -> dict:
 
 
 @app.post('/user/{username}/{age}')
-async def add_user(username: str, age: int) -> str:
+async def add_user(username: str = Path(min_length=5, max_length=20, description='Enter username',
+                                        example='UrbanUser'),
+                   age: int = Path(ge=18, le=120, description='Enter User ID', example='13')) -> str:
     new_user_id = str(int(max(users, key=int)) + 1)
     users[new_user_id] = f'Имя: {username}, возраст: {age}'
     return f'User {new_user_id} is registered.'
 
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: str, username: str, age: int) -> str:
+async def update_user(user_id: str,
+                      username: str = Path(min_length=5, max_length=20, description='Enter username',
+                                           example='NewUser'),
+                      age: int = Path(ge=18, le=120, description='Enter User ID', example='13')) -> str:
     users[user_id] = f'Имя: {username}, возраст: {age}'
     return f'User {user_id} has been updated.'
 
